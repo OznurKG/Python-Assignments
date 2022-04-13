dcf25c97549a4769d00a07babc3a91fac6c57301
OznurKG <101494438+OznurKG@users.noreply.github.com>
2022-04-13 23:18:13 +0300

    Update km_to_miles.py

--- km_to_miles.py
+++ km_to_miles.py
@@ -7,3 +7,5 @@ distance_km = input("Please enter a distance in km: ")
 distance_m = float(distance_km) * 0.621   # 1 km is equal to 0.621 miles
 print(f"{distance_km} kilometers is equal to {distance_m} miles.")
 
+
+
