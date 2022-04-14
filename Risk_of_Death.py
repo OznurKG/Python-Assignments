955e586b3fca5c788aecd2c326fb3b098a1feb1f
OznurKG <101494438+OznurKG@users.noreply.github.com>
2022-04-14 17:36:31 +0300

    Update Risk_of_Death.py

--- Risk_of_Death.py
+++ Risk_of_Death.py
@@ -9,7 +9,8 @@ Do you have a severe chronic disease? Variable → chronic
 
 Is your immune system too weak? Variable → immune
 
-Set a logical algorithm using boolean logic operators (and/or) and the given variables in order to give us True (there is a risk of death) or False (there is not a risk of death) as a result. 
+Set a logical algorithm using boolean logic operators (and/or) and the given variables in order to 
+give us True (there is a risk of death) or False (there is not a risk of death) as a result. 
 
 age = # can be assigned only True/False 
 chronic = # can be assigned only True/False 
@@ -18,7 +19,7 @@ risk = ? (True or False)
 
 """
 
-#Basic soluion:
+#Basic solution:
 
 age = False
 chronic = True
@@ -27,6 +28,27 @@ immune = True
 risk = chronic or immune or age
 print(risk)
 
+#Other solution: Here, we get the answers from the user, and determine whether there is a detah risk or not.
 
+age = input("You're a cigarette addict older than 75?: ") # Enter only "Yes" or "No"
+chronic = input("You have a severe chronic disease?: ")
+immune = input("Your immune system is too weak?: ")
+
+if age.capitalize() == "Yes" :   #To prevent errors due to the typing, we use the capitatize func.
+    age = True
+else : 
+    age = False
+if chronic.capitalize() == "Yes" :
+    chronic = True
+else :
+    chronic = False
+if immune.capitalize() == "Yes" :
+    immune = True
+else :
+    immune = False
+if chronic and immune and age:
+    print("You are in risky group")
+else :
+    print("You are not in risky group")
 
 
