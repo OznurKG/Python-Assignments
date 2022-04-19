583c4e3849736ec67f5890fd8a08796b66bec2c8
OznurKG <101494438+OznurKG@users.noreply.github.com>
2022-04-20 00:50:26 +0300

    Update ProfitCalculation.py

--- ProfitCalculation.py
+++ ProfitCalculation.py
@@ -1,5 +1,10 @@
-#1.Task. If you had deposited a coin on the cryptocurrency exchange that brought 7% fixed profit daily for a week, how much would your $ 1000 reach at the end of the 7th day?
+"""
+Task. If you had deposited a coin on the cryptocurrency exchange that brought 7% fixed profit daily for a week, 
+how much would your $ 1000 reach at the end of the 7th day?"""
+
 capital = 1000
 rate = 0.07
 capital = capital * (1 + rate)**7
+
 print(f"Your $1000 will reach ${round(capital)} at the end of the 7th day.")
+
