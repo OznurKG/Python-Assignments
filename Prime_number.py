9634eb33a099a5195d79c3c28c4ade7ed4ab12fd
oznur <oznurkgumus@gmail.com>
2022-04-23 00:10:06 +0300

    Second solution to the question.

--- Prime_number.py
+++ Prime_number.py
@@ -15,5 +15,20 @@ for n in range(2, number):
 else:
     print(f"{number} is a prime number")
         
+#Second solution:
 
+n = int(input("Enter a positive int number to check if it is a Prime Number."))
 
+counter = 0
+
+for i in range(1, n+1) :
+    if n % i == 0 :
+        counter += 1
+        
+if (n == 0) or (n == 1) or (counter >= 3) :
+    print(n, " is not a Prime Number.")
+    
+else :
+    print(n, " is a Prime Number.")
+
+    
